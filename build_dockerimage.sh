6ffebe3916ea5500f93b5baeb6d5d32c2b2a9e64
PauloASilva <pauloasilva@gmail.com>
2018-09-21 23:16:39 +0100

    Make container inherit host timezone
    Mounting /etc/locatime suffices

--- build_dockerimage.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-# builds docker image for TomTom Sportsconnect passing local timezone of host system
-# as an argument to inherit timezone of host system to docker image
-docker build --build-arg LOCALTIMEZONE=`cat /etc/timezone` -t tomtomsc:latest .
-
